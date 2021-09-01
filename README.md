2c69d961e8fe6512cb800e3a5e47bfef4e8d3eb8
cavebeavis <bkovacsconsulting@gmail.com>
2021-09-01 18:29:04 -0400

    feat(testing, readme, and modifications): Add testing to library, modify code after testing, and add to README.md - add logrus_test.go. - add zap_test.go. - add logType to both logrus and zap loggers like "json" for the formatted log type. - add CallerPrettyfier to logrus logger. - modify example/simple/main.go for new changes. - add to READNE.md with new image asset.

--- README.md
+++ README.md
@@ -1,5 +1,35 @@
+<div align="center"><img src="assets/images/small-log.png"></div>
+
 # Coolog
 
 Coolog is a logging interface based upon [Chris Hines' Slide Deck](https://go-talks.appspot.com/github.com/ChrisHines/talks/structured-logging/structured-logging.slide) and [gokit's log package](https://pkg.go.dev/github.com/go-kit/kit@v0.10.0/log).
 
-The motivation for this package can be found here: https://briankovacs.medium.com/golang-interfaces-60e459ebd7c0
+The motivation for this package can be [found here](https://briankovacs.medium.com/golang-interfaces-60e459ebd7c0).
+
+This library is meant as a common interface to be able to implement any type of log package like github.com/sirupsen/logrus or go.uber.org/zap.
+
+So where you need a logger in your code, do this:
+``` go
+func funcWhichPassesLogger(log *coolog.Logger) {
+
+}
+```
+
+That way, any logger which implements this logging interface is good to go. Using a different kind of method than we are used to:
+``` go
+Print(level string, msg string, data ...map[string]interface{}) error
+```
+
+So to call the logger, we need to pass the level (which has defaults depending upon the implementation currently in this library -- aka zap or logrus), the message string, and and detail fields.
+``` go
+logLevel := "info"
+logFilename := "/var/log/your_log_mang.log"
+logType := "text"
+
+log, err := coolog.NewLogrusLogger(logLevel , logFilename, logType)
+if err != nil {
+    // handle d stuff mang!
+}
+
+log.Print("error", "this is a serious thing you did wrong, mang", map[string]interface{}{"key1":"value1", "key2":"value2"})
+```
--- zap.go
+++ zap.go
@@ -1,85 +1,202 @@
 package coolog
 
 import (
-    "go.uber.org/zap"
-    "go.uber.org/zap/zapcore"
+	"time"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
-// ZapLogger is our struct to implement the Logger interface.
+/*
+    https://pkg.go.dev/go.uber.org/zap#Config
+    https://play.golang.org/p/8PTI83OA8XZ
+    // For some users, the presets offered by the NewProduction, NewDevelopment,
+	// and NewExample constructors won't be appropriate. For most of those
+	// users, the bundled Config struct offers the right balance of flexibility
+	// and convenience. (For more complex needs, see the AdvancedConfiguration
+	// example.)
+	//
+	// See the documentation for Config and zapcore.EncoderConfig for all the
+	// available options.
+	rawJSON := []byte(`{
+	  "level": "debug",
+	  "encoding": "json",
+	  "outputPaths": ["stdout", "/tmp/logs"],
+	  "errorOutputPaths": ["stderr"],
+	  "initialFields": {"foo": "bar"},
+	  "encoderConfig": {
+	    "messageKey": "message",
+	    "levelKey": "level",
+	    "levelEncoder": "lowercase"
+	  }
+	}`)
+	var cfg zap.Config
+	if err := json.Unmarshal(rawJSON, &cfg); err != nil {
+		panic(err)
+	}
+	logger, err := cfg.Build()
+	if err != nil {
+		panic(err)
+	}
+	defer logger.Sync() // flushes buffer, if any
+*/
+
+/*
+	https://pkg.go.dev/go.uber.org/zap@v1.17.0/zapcore#EncoderConfig
+	type EncoderConfig struct {
+		// Set the keys used for each log entry. If any key is empty, that portion
+		// of the entry is omitted.
+		MessageKey    string `json:"messageKey" yaml:"messageKey"`
+		LevelKey      string `json:"levelKey" yaml:"levelKey"`
+		TimeKey       string `json:"timeKey" yaml:"timeKey"`
+		NameKey       string `json:"nameKey" yaml:"nameKey"`
+		CallerKey     string `json:"callerKey" yaml:"callerKey"`
+		FunctionKey   string `json:"functionKey" yaml:"functionKey"`
+		StacktraceKey string `json:"stacktraceKey" yaml:"stacktraceKey"`
+		LineEnding    string `json:"lineEnding" yaml:"lineEnding"`
+		// Configure the primitive representations of common complex types. For
+		// example, some users may want all time.Times serialized as floating-point
+		// seconds since epoch, while others may prefer ISO8601 strings.
+		EncodeLevel    LevelEncoder    `json:"levelEncoder" yaml:"levelEncoder"`
+		EncodeTime     TimeEncoder     `json:"timeEncoder" yaml:"timeEncoder"`
+		EncodeDuration DurationEncoder `json:"durationEncoder" yaml:"durationEncoder"`
+		EncodeCaller   CallerEncoder   `json:"callerEncoder" yaml:"callerEncoder"`
+		// Unlike the other primitive type encoders, EncodeName is optional. The
+		// zero value falls back to FullNameEncoder.
+		EncodeName NameEncoder `json:"nameEncoder" yaml:"nameEncoder"`
+		// Configures the field separator used by the console encoder. Defaults
+		// to tab.
+		ConsoleSeparator string `json:"consoleSeparator" yaml:"consoleSeparator"`
+	}
+*/
+
+// ZapLogger is the struct which implements the Logger interface which allows access to the
+// "go.uber.org/zap" style of logging.
 type ZapLogger struct {
-    Logger  *zap.Logger
-    Level   string
-    Atom    zap.AtomicLevel
+	Logger *zap.Logger
+	Level  string
+	Atom   zap.AtomicLevel
 }
 
-// NewZapLogger is a convenience function to create the ZapLogger.
-func NewZapLogger(level string, logLocations []string) (*ZapLogger, error) {
-    encoderCfg := zapcore.EncoderConfig{
-        MessageKey:    "msg",
-        LevelKey:      "level",
-        TimeKey:       "@ts",
-        CallerKey:     "caller",
-        FunctionKey:   "func",
-        StacktraceKey: "stack",
-        LineEnding:    ",\n",  
-        EncodeLevel:    zapcore.LowercaseLevelEncoder,
-        EncodeTime:     zapcore.TimeEncoderOfLayout("2006-01-02 15:04:05.000000000 MST"),
-        EncodeDuration: zapcore.NanosDurationEncoder,
-        EncodeCaller:   zapcore.FullCallerEncoder,
-    }
-    zapLevel := zap.InfoLevel
-    switch level {
-    case "trace", "debug":
-        zapLevel = zap.DebugLevel
-    case "info":
-        zapLevel = zap.InfoLevel
-    case "warn":
-        zapLevel = zap.WarnLevel
-    case "error":
-        zapLevel = zap.ErrorLevel
-    }
-    atom := zap.NewAtomicLevelAt(zapLevel)
-    cfg := zap.Config{
-        EncoderConfig: encoderCfg,
-        Level: atom,
-        Encoding: "json",
-        OutputPaths: logLocations,
-        ErrorOutputPaths: logLocations,
-    }
-    logger, err := cfg.Build()
-    if err != nil {
-        return nil, err
-    }
-    return &ZapLogger{Logger: logger, Atom: atom, Level: level}, nil
+// NewZapLogger provides a newly initialized Zap Logger which implements Logger interface.
+//
+// level is "trace", "debug", "info", "warn", "error" which will be the minimum level
+// this Logger will log in the file or console. If level is empty, it will default to
+// "info" level.
+//
+// logLocations s where you want the log file stored. If this is empty, it will default
+// to stdout.
+//
+// logType is "json" or "text". If this is empty, it will default to "json".
+func NewZapLogger(level string, logLocations []string, logType string) (*ZapLogger, error) {
+	encoderCfg := zapcore.EncoderConfig{
+		MessageKey:    "msg",
+		LevelKey:      "level",
+		TimeKey:       "@timestamp", // For ElasticSearch: https://www.elastic.co/guide/en/ecs/current/ecs-base.html#field-timestamp
+		//NameKey       string `json:"nameKey" yaml:"nameKey"`
+		CallerKey:     "caller",
+		FunctionKey:   "func",
+		StacktraceKey: "stack",
+		LineEnding:    ",\n",
+		// Configure the primitive representations of common complex types. For
+		// example, some users may want all time.Times serialized as floating-point
+		// seconds since epoch, while others may prefer ISO8601 strings.
+		EncodeLevel:    zapcore.LowercaseLevelEncoder,
+		EncodeTime:     zapcore.TimeEncoderOfLayout(time.RFC3339Nano), // TODO: make this user configurable.
+		EncodeDuration: zapcore.NanosDurationEncoder,
+		EncodeCaller:   zapcore.FullCallerEncoder,
+		// Unlike the other primitive type encoders, EncodeName is optional. The
+		// zero value falls back to FullNameEncoder.
+		//EncodeName NameEncoder `json:"nameEncoder" yaml:"nameEncoder"`
+		// Configures the field separator used by the console encoder. Defaults
+		// to tab.
+		//ConsoleSeparator string `json:"consoleSeparator" yaml:"consoleSeparator"`
+	}
+
+	zapLevel := zap.InfoLevel
+	switch level {
+	case "trace", "debug":
+		zapLevel = zap.DebugLevel
+	case "info":
+		zapLevel = zap.InfoLevel
+	case "warn":
+		zapLevel = zap.WarnLevel
+	case "error":
+		zapLevel = zap.ErrorLevel
+	}
+
+	atom := zap.NewAtomicLevelAt(zapLevel)
+
+	for i, location := range logLocations {
+		if location == "console" || location == "" {
+			logLocations[i] = "stdout"
+		}
+	}
+
+	switch logType {
+	case "txt", "text", "plain":
+		logType = "console"
+	case "console":
+		// fall through
+	default:
+		logType = "json"
+	}
+
+	cfg := zap.Config{
+		EncoderConfig:    encoderCfg,
+		Level:            atom,
+		Encoding:         logType,
+		OutputPaths:      logLocations,
+		ErrorOutputPaths: logLocations,
+	}
+
+	logger, err := cfg.Build()
+	if err != nil {
+		return nil, err
+	}
+
+	return &ZapLogger{Logger: logger, Atom: atom, Level: level}, nil
+
 }
 
 // Print is the main method, and implements interface Logger.
 func (z *ZapLogger) Print(level string, msg string, data ...map[string]interface{}) error {
-    var fields []zap.Field
-	for _, d := range data {
-		for k, v := range d {
-			field := zap.Any(k, v)
-			fields = append(fields, field)
+	//TODO: get rid of Any and do this with true zap fields from the calling function...
+	var fields []zap.Field
+
+	if len(data) > 0 {
+		for _, dataMap := range data {
+			if dataMap == nil {
+				continue
+			}
+
+			for k, v := range dataMap {
+				fields = append(fields, zap.Any(k, v))
+			}
 		}
 	}
-    
-    switch level {
-    case "trace", "debug":
-        z.Logger.Debug(msg, fields...)
-    case "info":
-        z.Logger.Info(msg, fields...)
-    case "warn":
-        z.Logger.Warn(msg, fields...)
-    case "error":
-       z.Logger.Error(msg, fields...)
-    default:
-       z.Logger.Error(msg, fields...)
-    }
-    return nil
+
+	switch level {
+	case "trace", "debug":
+		z.Logger.Debug(msg, fields...)
+	case "info":
+		z.Logger.Info(msg, fields...)
+	case "warn":
+		z.Logger.Warn(msg, fields...)
+	case "error":
+		z.Logger.Error(msg, fields...)
+	case "fatal":
+		z.Logger.Fatal(msg, fields...)
+	case "panic":
+		z.Logger.Panic(msg, fields...)
+	default:
+		z.Logger.Error(msg, fields...)
+	}
+
+	return nil
 }
 
-// Close is following the examples in zap -- may be a better way to
-// do this since the caller of NewZapLogger needs to defer z.Close().
+// Close will perform the Logger.Sync() specifically for zap.
 func (z *ZapLogger) Close() error {
-    return z.Logger.Sync()
+	return z.Logger.Sync()
 }
